cf8563f25a8aca41c1119eac2498d9f43ff346a6
Vankadara <Rajendra_Kumar_Vanka@Dell.com>
2021-04-15 12:49:38 +0530

    Phase1 task

---
+++ source/repos/Phase1_Project/Phase1_Project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileText = @"C:\Users\rajendra_kumar_vanka\source\repos\Phase1_Project\Storing_Updating_Teacher_Data.txt";
+
+            List<string> teachersdata = new List<string>();
+            teachersdata = File.ReadAllLines(fileText).ToList();
+            Console.WriteLine("Rainbow school teachers data: ");
+            Console.WriteLine("Enter Teacher ID : ");
+            var Id = Console.ReadLine();
+            Console.WriteLine("Enter Teacher Name : ");
+            var Name = Console.ReadLine();
+            Console.WriteLine("Enter Teacher Class : ");
+            var Class = Console.ReadLine();
+            Console.WriteLine("Enter Teacher Section : ");
+            var Section = Console.ReadLine();
+            teachersdata.Add(@"Id : " + Id.ToString() + ", Name : " + Name + ", Class : " + Class + ", Section : " + Section);
+            File.WriteAllLines(fileText, teachersdata);
+            //retrieving teacher data from text file.
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Existing teacher data in Rainbow school : ");
+            for (int i = 0; i < teachersdata.Count(); i++)
+            {
+                Console.WriteLine(teachersdata[i]);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
